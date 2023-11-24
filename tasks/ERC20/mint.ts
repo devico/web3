064d6674fb1410ff2251e2946f5b8d247021497b
devico <clamdm@gmail.com>
2023-11-24 16:17:51 +0200

    fixed tasks

--- tasks/ERC20/mint.ts
+++ tasks/ERC20/mint.ts
@@ -3,15 +3,16 @@ import { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types"
 import { ERC20 } from "../../typechain"
 
 task("mint", "Mints new tokens")
-    .addParam("amount", "The amount of tokens to mint")
-    .setAction(async (taskArgs: TaskArguments, hre: HardhatRuntimeEnvironment): Promise<void> => {
-        const [minter] = await hre.ethers.getSigners();
-        
-        const token: ERC20 = <ERC20>(
-          await hre.ethers.getContractAt("ERC20", "0xb2371A6b319098B7B150f57040209c91e4943537")
-        )
-        
-        await token.connect(minter).mint(taskArgs.amount);
-        
-        console.log(`Minted ${taskArgs.amount} tokens`);
-    });
+  .addParam("account", "The account's address")
+  .addParam("amount", "The amount of tokens to mint")
+  .setAction(async (taskArgs: TaskArguments, hre: HardhatRuntimeEnvironment): Promise<void> => {
+    const [minter] = await hre.ethers.getSigners();
+
+    const token: ERC20 = <ERC20>(
+      await hre.ethers.getContractAt("ERC20", "0xb2371A6b319098B7B150f57040209c91e4943537")
+    )
+
+    await token.connect(minter).mint(taskArgs.amount, taskArgs.amount);
+
+    console.log(`Minted ${taskArgs.amount} tokens`);
+  });
